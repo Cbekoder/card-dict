bc3eb0bcc99a4c1316855c9f3f369416ca2ca351
cbekoder <cbekoder@gmail.com>
2025-03-04 11:10:31 +0500

    adding retrieve dictionar

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import DictionaryListCreateAPIView, redirect_to_docs
+from .views import DictionaryListCreateAPIView, redirect_to_docs, DictionaryRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('', redirect_to_docs),
     path('dictionaries/', DictionaryListCreateAPIView.as_view(), name='dictionary-list-create'),
+    path('dictionaries/<int:pk>/', DictionaryRetrieveUpdateDestroyAPIView.as_view(), name='dictionary-detail'),
 ]
