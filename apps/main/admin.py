80831ff2120d55ca5050d6271bf5908c86191cae
cbekoder <cbekoder@gmail.com>
2025-03-04 10:42:08 +0500

    adding admin.py

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dictionary
 
-# Register your models here.
+@admin.register(Dictionary)
+class DictionaryAdmin(admin.ModelAdmin):
+    list_display = ('word', 'translation', 'base_lang', 'target_lang', 'user')  # Show in list view
+    search_fields = ('word', 'translation', 'user')  # Add search functionality
+    list_filter = ('base_lang', 'target_lang')  # Add filters for better navigation
+    ordering = ('word',)  # Order by word alphabetically
