07177748be80379cab84218720a008692611de1e
cbekoder <cbekoder@gmail.com>
2025-03-04 11:13:26 +0500

    update token urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from .views import RegisterView, GetMeView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('get-me/', GetMeView.as_view(), name='get-me')
 ]
